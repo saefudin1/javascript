332560c901bbdb95c33131d5a6688edbb816d8b3
saefudin1 <saefudin1@users.noreply.github.com>
2015-08-06 10:30:27 +0700

    Create nongkongnews.js

---
+++ nongkongnews.js
@@ -0,0 +1,12 @@
+function Barva(koda)
+{
+document.getElementById(&quot;vzorec&quot;).bgColor=koda;
+document.hcc.barva.value=koda.toUpperCase();
+document.hcc.barva.select();
+}
+function BarvaDruga(koda)
+{
+document.getElementById(&quot;vzorec2&quot;).bgColor=koda;
+document.hcc.Barva2.value=koda.toUpperCase();
+document.hcc.Barva2.select();
+}
